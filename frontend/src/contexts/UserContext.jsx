b1b472d995553bd00bde5bb37c011f89f4f9ad86
Atta Ullah <attaullah41531@gmail.com>
2026-04-13 18:32:04 +0500

    fixed

--- frontend/src/contexts/UserContext.jsx
+++ frontend/src/contexts/UserContext.jsx
@@ -1,3 +1,4 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
 import { login as apiLogin, logout as apiLogout, getMe } from '../services/api';
 
 const UserContext = createContext();
